d792b0b32ce226dac22effe86b9db83abe25f761
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2022-03-04 16:15:04 +0500

    created transfer through procedure

--- model/model.sql
+++ model/model.sql
@@ -7,4 +7,27 @@ account_budget DECIMAL(10, 2) NOT NULL
 INSERT INTO accounts (account_name, account_budget) VALUES ('Shoxrux', 500000);
 INSERT INTO accounts (account_name, account_budget) VALUES ('Abduhalim', 100000);
 
-CREATE OR REPLACE PROCEDURE 
+CREATE OR REPLACE PROCEDURE transfer(senderId int, receiverId int, amount int)
+LANGUAGE plpgsql
+AS
+$$
+BEGIN
+  IF
+    (
+      (SELECT account_id FROM accounts WHERE account_id = senderId) IS NULL
+    OR
+      (SELECT account_id FROM accounts WHERE account_id = receiverId ) IS NULL    
+    )
+  THEN 
+    RAISE EXCEPTION 'Sender / Receiver not found☹';
+  ELSIF
+    ((SELECT account_budget FROM accounts WHERE account_id = senderId ) < amount)
+  THEN 
+    RAISE EXCEPTION 'You have less budget☹';
+  ELSE
+    UPDATE accounts SET account_budget = account_budget - amount WHERE account_id = senderId;
+    UPDATE accounts SET account_budget = account_budget + amount WHERE account_id = receiverId;
+    RAISE INFO 'Money successfully passed to %!', (SELECT account_name FROM accounts WHERE account_id = receiverId );
+  END IF;
+END;
+$$;
